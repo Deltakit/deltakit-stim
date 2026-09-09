fba3905d42b4e9bd5f80c839b60ea673e7c744cb
Roland Guichard <roland.guichard@riverlane.com>
2026-09-09 14:54:06 +0100

    Better test.

--- src/stim/py/stim_pybind_test.py
+++ src/stim/py/stim_pybind_test.py
@@ -306,14 +306,16 @@ def test_target_combined_paulis():
 def test_deltakit_stim_stim_compatibility():
     """Show that deltakit-stim can be imported after stim without their python bindings conflicting"""
     code = """
-    import stim, deltakit_stim
+import stim, deltakit_stim
 
-    c = deltakit_stim.Circuit()
-    for inst in c: print(inst)
-    """
-    subprocess.run(
+c = deltakit_stim.Circuit()
+for inst in c: print(inst)
+"""
+    result = subprocess.run(
         [sys.executable, "-c", code],
         capture_output=True,
         text=True,
         check=True,
     )
+
+    assert result.returncode == 0
